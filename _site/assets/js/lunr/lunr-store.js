b810fc3b270df31942d9263f186bf94392ebba84
gloomy16 <gloomysixteen@gmail.com>
2018-06-21 07:14:02 +0700

    new configuration wdm & metadata

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Kenalan Yuk !",
+        "excerpt":"Hi semuanya, perkenalkan nama saya Ilham Saputrajati biasa di panggil ilham.Hobi saya adalah Programming (Build Aplikasi Android &amp; Website), saya menyukai olahraga Juggle &amp; Flair Bartending .Saya adalah pribadi yang mempunyai semangat belajar dan motivasi yang tinggi saya tidak ingin kalah dalam hal ini.Cita-cita saya menjadi seorang Bupati di Gunungkidul,...","categories": ["blog"],
+        "tags": ["perkenalan"],
+        "url": "http://localhost:4000/blog/pekenalan/",
+        "teaser":null}]
