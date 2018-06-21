6d7e8532016efe112fbc4c3ec03d17ae806952b5
gloomy16 <gloomysixteen@gmail.com>
2018-06-21 08:53:50 +0700

    configuration teaser

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,4 +3,4 @@ var store = [{
         "excerpt":"Hi semuanya, perkenalkan nama saya Ilham Saputrajati biasa di panggil ilham.Hobi saya adalah Programming (Build Aplikasi Android &amp; Website), saya menyukai olahraga Juggle &amp; Flair Bartending .Saya adalah pribadi yang mempunyai semangat belajar dan motivasi yang tinggi saya tidak ingin kalah dalam hal ini.Cita-cita saya menjadi seorang Bupati di Gunungkidul,...","categories": ["blog"],
         "tags": ["perkenalan"],
         "url": "http://localhost:4000/blog/pekenalan/",
-        "teaser":"http://localhost:4000/assets/images/profil%202.png"}]
+        "teaser":"http://localhost:4000/assets/images/teaser.jpg"}]
