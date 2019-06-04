67f00ef1bd120630d2153180de44d3ec181733a8
ilhamsj <ilhamsaputrajati@gmail.com>
2019-06-05 00:30:36 +0700

    before merge

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,19 +3,19 @@ var store = [{
         "excerpt":"                                                                                                  Silahkan di download Logo Universitas PGRI Yogyakarta.  ","categories": ["blog","logo"],
         "tags": ["Universitas PGRI Yogyakarta","Logo"],
         "url": "http://localhost:4000/blog/logo/logo-universitas-pgri-yogyakarta/",
-        "teaser":"http://localhost:4000/assets/images/teaser.jpg"},{
+        "teaser":null},{
         "title": "Ilham Saputra Jati - Solve Problems with technology",
         "excerpt":"Hi semuanya, perkenalkan nama saya Ilham Saputrajati biasa di panggil ilham.Hobi saya Programming Juggle &amp; Flair Bartending . Main gitarSaya adalah pribadi yang mempunyai semangat belajar dan motivasi yang tinggi.Cita-cita saya menjadi orang yang bermanfaat orang lain sesuai dengan bidang studi saya (Engineer, Dosen, CEO, Bupati) Saya sangat mencintai teknologi,...","categories": ["blog"],
         "tags": ["perkenalan"],
         "url": "http://localhost:4000/blog/pekenalan/",
-        "teaser":"http://localhost:4000/assets/images/teaser.jpg"},{
+        "teaser":null},{
         "title": "Cara menggunakan smartphone sebagai clicker",
         "excerpt":"Hallo semuanya, teman teman pasti pernah melakukan presentasi, ketika presentasi untuk mengendalikan slide after/previous slide biasanya menggunakan keyboard.akan tetapi ketika kita presentasi dalam acara acara penting seperti seminar, workshop, presentasi pitch desk ke investor tidak mungkin menggunakan keyboard karena sangat tidak terlihat professional. biasanya orang orang menggunakan clicker seperti ini...","categories": ["blog"],
         "tags": ["Tutorial"],
         "url": "http://localhost:4000/blog/menggunakan-smartphone-sebagai-clicker/",
-        "teaser":"http://localhost:4000/assets/images/teaser.jpg"},{
+        "teaser":null},{
         "title": "Kumpulan nama nama bayi terinspirasi dari bahasa pemrograman, framework dan tools",
         "excerpt":"Hi developers, kali ini saya bakalan sharing nama nama bayi yang terinspirasi dari bahasa pemrograman             Bahasa Pemrograman      Nama                  SQL      Ezkiel              Vim      Vim              Vue      Vue              Sass      Sasa              React      Rea              Swift      swifty              Js      Jessy              Git      gita              Laravel      Lara              Debian      Debi              CSS      Cassy              Jekyll      Jackie      ","categories": ["blog"],
         "tags": ["Tutorial"],
         "url": "http://localhost:4000/blog/bank-nama-bayi-terinspirasi-bahasa-pemrograman/",
-        "teaser":"http://localhost:4000/assets/images/teaser.jpg"}]
+        "teaser":null}]
